263266c30ba35f482d053f9241e33c4d2bd62759
rohrig <richard.t.rohrig@gmail.com>
2023-06-19 20:56:20 +0200

    feat(layout): add layout

--- apps/nuxt/app.vue
+++ apps/nuxt/app.vue
@@ -1,23 +1,5 @@
 <template>
-  <div>
-    <div>
-      <SfButton @click="submit" type="button" class="w-full"> Hello </SfButton>
-    </div>
-    <div class="flex flex-col align-center">
-     <div v-for="product in products" :key="product.uid">
-       <div> {{ product.name }} </div>
-     </div>
-    </div> 
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
-
-<script lang="ts" setup>
-import { SfButton } from '@storefront-ui/vue';
-import { ProductInterface } from '@vue-storefront/magento-types';
-
-const {search} = useProduct()
-const submit = () => {
-    search('watch')
-}
-const products = useState<ProductInterface[]>('products')
-</script>
---
+++ apps/nuxt/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <TheTopNav/>
+        <slot/>
+    </div>
+</template>
+<script setup lang="ts">
+import TheTopNav from "~/components/blocks/layout/TheTopNav.vue";
+</script>
