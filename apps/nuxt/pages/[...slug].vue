d52f517ab2a5c7ecfc8ca298b3933c308305105e
rohrig <richard.t.rohrig@gmail.com>
2023-06-21 18:40:19 +0200

    feat(routing): set up global route middleware and initialze page based on route type

---
+++ apps/nuxt/pages/[...slug].vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <component :is="type" v-if="type" />
+  </div>
+</template>
+  
+<script lang="ts" setup>
+
+import CATEGORY from '~/modules/catalog/category/pages/index.vue';
+import PRODUCT from '~/modules/catalog/product/pages/index.vue';
+
+const category = useState<{ type: string }>('routeData')?.value?.type
+const type = ref();
+
+if (category == 'PRODUCT') {
+  type.value = PRODUCT
+}
+
+if (category == 'CATEGORY') {
+  type.value = CATEGORY
+}
+</script>
+  
+  
