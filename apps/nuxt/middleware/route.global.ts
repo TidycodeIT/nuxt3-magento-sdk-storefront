e30875dcd52ff8cf9a929f4af88b34a2281b86ca
rohrig <richard.t.rohrig@gmail.com>
2023-06-21 18:49:46 +0200

    feat(routing): set up global route middleware and initialze page based on route type

--- apps/nuxt/middleware/route.global.ts
+++ apps/nuxt/middleware/route.global.ts
@@ -1,12 +1,6 @@
 import { sdk } from "~/sdk.config"
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
-  console.log('middleware/path: ', to.path)
-
   const mageRoute = await sdk.magento.route({ url: to.path })
-
-  console.log('route/middleware', mageRoute?.data?.route)
-
   useState('routeData', () => mageRoute?.data?.route)
-
 })
