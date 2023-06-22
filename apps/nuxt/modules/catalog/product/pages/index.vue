fab65dcccfab24e5d1cfec826068d81674c9c812
rohrig <richard.t.rohrig@gmail.com>
2023-06-22 02:15:33 +0200

    feat(pdp): get info from magento

--- apps/nuxt/modules/catalog/product/pages/index.vue
+++ apps/nuxt/modules/catalog/product/pages/index.vue
@@ -1,13 +1,6 @@
 <template>
   <section class="flex items-center gap-8 flex-wrap p-6">
     <div>
-      <!-- <div>
-        Product: {{ product }}
-      </div>
-      <div>
-        Product Details: {{ productDetails }}
-      </div> -->
-      
       <ProductVerticalGallery v-if="productDetails.data && product.data" :productDetails="productDetails.data" :product="product.data"
         class="!w-auto" />
     </div>
