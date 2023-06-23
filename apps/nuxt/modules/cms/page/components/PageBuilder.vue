a9846e504a8d0cbcd3664774bdf29327d5e49471
PhantomDraven <kevin.statua@gmail.com>
2023-06-23 10:27:35 +0200

    Feature/homepage (#28)
    * Add configuration to enable/disable Pagebuilder and DemoContent of homepage
    
    * Created CmsPage module - homepage use the CmsPage module - added demo content and empty file for PageBuilder integration
    
    * Add style for big screens
    
    * feat(homepage): fomatting
    
    ---------
    
    Co-authored-by: rohrig <richard.t.rohrig@gmail.com>

---
+++ apps/nuxt/modules/cms/page/components/PageBuilder.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>Pagebuilder</div>
+</template>
+
+<script lang="ts" setup>
+</script>
--- apps/nuxt/nuxt.config.ts
+++ apps/nuxt/nuxt.config.ts
@@ -6,5 +6,10 @@ export default defineNuxtConfig({
     private: {
       magentoBaseUrl: process.env.MAGENTO_BASE_URL,
       imageProvider: process.env.NUXT_IMAGE_PROVIDER,
-    }}
+    },
+    public: {
+      demoContent: process.env.VSF_DEMO_CONTENT,
+      pageBuilder: process.env.VSF_PAGE_BUILDER_MODULE
+    }
+  }
 })
