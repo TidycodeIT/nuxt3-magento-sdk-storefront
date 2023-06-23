caad741f6168d652748db517786d70dabc36280e
PhantomDraven <kevinstatua@gmail.com>
2023-06-23 09:51:43 +0200

    Created CmsPage module - homepage use the CmsPage module - added demo content and empty file for PageBuilder integration

--- apps/nuxt/pages/index.vue
+++ apps/nuxt/pages/index.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
-    home page
+    <CMS_PAGE :content="pageData.content"></CMS_PAGE>
   </div>
 </template>
+
+<script lang="ts" setup>
+import CMS_PAGE from '~/modules/cms/page/pages/index.vue';
+import {sdk} from "~/sdk.config";
+
+const { data: { cmsPage: pageData }, pending, refresh } = await sdk.magento.cmsPage({
+  identifier: 'home'
+})
+
+useHead({
+  title: pageData.meta_title || pageData.title,
+  meta: [{
+    name: 'description',
+    content: pageData.description
+  },{
+    name: 'keywords',
+    content: pageData.meta_keywords
+  }]
+})
+</script>
