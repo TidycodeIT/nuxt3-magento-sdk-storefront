e689e00a50bf2cd2e13dc78af315a325f58f1145
Faizan Arif <faizanarif121@gmail.com>
2023-06-25 08:15:25 +0500

    style(footer): make footer bottom bar full width on all screens

--- apps/nuxt/components/blocks/layout/TheFooter.vue
+++ apps/nuxt/components/blocks/layout/TheFooter.vue
@@ -38,37 +38,39 @@
         </p>
       </div>
     </div>
-    <div class="bg-neutral-900 justify-end px-4 py-10 md:flex md:py-6 w-full">
-      <div class="flex justify-center py-2 gap-x-4 md:self-start">
-        <SfButton
-          v-for="{ label, link, icon } in socialMedia"
-          :key="label"
-          tag="a"
-          square
-          variant="tertiary"
-          class="text-white active:text-white hover:text-white hover:!bg-neutral-500 active:!bg-transparent"
-          :href="link"
-          :aria-label="`Go to ${label} page`"
+    <div class="bg-neutral-900 w-full">
+      <div class="justify-end px-4 py-10 md:flex md:p-6 md:mx-auto max-w-[1536px]">
+        <div class="flex justify-center py-2 gap-x-4 md:self-start">
+          <SfButton
+            v-for="{ label, link, icon } in socialMedia"
+            :key="label"
+            tag="a"
+            square
+            variant="tertiary"
+            class="text-white active:text-white hover:text-white hover:!bg-neutral-500 active:!bg-transparent"
+            :href="link"
+            :aria-label="`Go to ${label} page`"
+          >
+            <component :is="icon" />
+          </SfButton>
+        </div>
+        <div class="flex items-center justify-center gap-6 py-2 my-4 md:ml-auto md:my-0">
+          <SfLink
+            v-for="{ label, link } in bottomLinks"
+            :key="label"
+            variant="secondary"
+            class="text-white no-underline typography-text-sm active:text-white active:underline hover:text-white hover:underline"
+            :href="link"
+          >
+            {{ label }}
+          </SfLink>
+        </div>
+        <p
+          class="flex items-center justify-center py-2 leading-5 text-center typography-text-sm text-white/50 font-body md:ml-6"
         >
-          <component :is="icon" />
-        </SfButton>
+          @2023 Vue Storefront
+        </p>
       </div>
-      <div class="flex items-center justify-center gap-6 py-2 my-4 md:ml-auto md:my-0">
-        <SfLink
-          v-for="{ label, link } in bottomLinks"
-          :key="label"
-          variant="secondary"
-          class="text-white no-underline typography-text-sm active:text-white active:underline hover:text-white hover:underline"
-          :href="link"
-        >
-          {{ label }}
-        </SfLink>
-      </div>
-      <p
-        class="flex items-center justify-center py-2 leading-5 text-center typography-text-sm text-white/50 font-body md:ml-6"
-      >
-        @2023 Vue Storefront
-      </p>
     </div>
   </footer>
 </template>
