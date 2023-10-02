11879323c25683ce2d2520c576aa4770857ac58e
tidypol <pol@tidycode.it>
2023-10-02 17:50:40 +0200

    testing add to cart sdk

--- apps/nuxt/modules/checkout/composables/useCart.ts
+++ apps/nuxt/modules/checkout/composables/useCart.ts
@@ -1,20 +1,44 @@
 import {Product, ProductInterface} from '@vue-storefront/magento-types';
 import {sdk} from "~/sdk.config";
 
+
 export const useCart = () => {
-    const addToCart = async (product: Product, quantity: number) => {
+    const cartIDCookie = useCookie('cart-id')
+    const getCartId=async ()=>{
+        //TODO implement auth customer
+        /*const retrivedCart = sdk.magento.customerCart()
+        console.log(retrivedCart)*/
+        if(cartIDCookie.value){
+            return cartIDCookie.value
+        }
+        const emptyCart = await sdk.magento.createEmptyCart()
+        const cartID = emptyCart.data?.createEmptyCart
+        if(cartID){
+            cartIDCookie.value = cartID
+        }
+        return cartIDCookie.value
+    }
+    const addToCart = async (product: Product, quantity: number=1) => {
+
         console.log(product)
         if (product.sku) {
             const {data, pending, refresh} = await useAsyncData('data', async () => {
-                console.log('refreshing products')
-                return await sdk.magento.addProductsToCart({
-                    cartId: "",
-                    cartItems: {
-                        // @ts-ignore
-                        sku: product.sku,
-                        quantity
-                    }
-                })
+                const cartID = await getCartId()
+                if(cartID){
+
+                    return await sdk.magento.addProductsToCart({
+                        cartId: cartID,
+                        cartItems: [
+                            {
+                                // @ts-ignore
+                                sku: product.sku,
+                                quantity:Number(quantity),
+
+                            }
+                        ]
+                    })
+                }
+                return undefined
             })
         }
 
